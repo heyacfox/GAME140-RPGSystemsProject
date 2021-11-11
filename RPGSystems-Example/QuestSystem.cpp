3a13ef75041b22caf80ee68adb6f04b662d28b14
Vaughn Reis <Vaughn Reis@DESKTOP-BMVC8PM>
2021-11-11 11:51:25 -0500

    Initial System

--- RPGSystems-Example/QuestSystem.cpp
+++ RPGSystems-Example/QuestSystem.cpp
@@ -1,6 +1,31 @@
 #include "QuestSystem.h"
+#include <stdio.h>
+#include <string>
 
+QuestSystem::QuestSystem()
+{
+	std::vector<int> numberOfQuests = { 1 };
+	std::string activeQuest = "Quest1";
+}
 
+void QuestSystem::CheckSystem() {
+	printf("SomeSystem has been checked with an int of %d!\n", numberOfQuests);
+}
+
+void QuestSystem::AddToInt() {
+	numberOfQuests = numberOfQuests + 1;
+}
+
+void QuestSystem::ActivateQuest() {
+
+}
+
+void TestQuestSystem() {
+	QuestSystem someSystemInstance = QuestSystem();
+	someSystemInstance.CheckSystem();
+	someSystemInstance.AddToInt();
+	someSystemInstance.CheckSystem();
+}
 
 
 
--- RPGSystems-Example/QuestSystem.h
+++ RPGSystems-Example/QuestSystem.h
@@ -1,4 +1,20 @@
 #pragma once
 #include <stdio.h>
+#include <string>
+#include <vector>
+
+
+class QuestSystem {
+
+public:
+	QuestSystem();
+	virtual void CheckSystem();
+	virtual void AddToInt();
+	virtual void ActivateQuest();
+private:
+	std::vector < std::string > numberOfQuests;
+	
+
+};
 
 void TestQuestSystem();
