a20045237e113aec5e445b0292504ab927b08e92
Mariettegg <100428980@alumnos.uc3m.es>
2021-11-11 11:45:07 -0500

    Mostly done

--- RPGSystems-Example/SkillsSystem.cpp
+++ RPGSystems-Example/SkillsSystem.cpp
@@ -2,12 +2,72 @@
 
 
 
+SkillSystem::SkillList()
+{
 
+}
+
+
+Skill::Skill(std::string Name)
+{
+	name = Name;
+	lvl = 1;
+	uses = 0;
+}
+
+void AddSkill(std::vector <Skill> SkillList, std::string name)
+{
+	Skill MySkill = Skill(name);
+	for (unsigned int i = 0; i < SkillList.size(); ++i)
+	{
+		if (SkillList[i].name == name)
+		{
+			printf("You already have this skill\n");
+			return;
+		}
+	}
+	SkillList.push_back(MySkill);
+}
+
+void  UseSkill(std::vector <Skill> SkillList, Skill& skill)
+{
+	bool found = false;
+
+	for (unsigned int i = 0; i < SkillList.size(); ++i)
+	{
+		if (SkillList[i].name == skill.name)
+		{
+			found = true;
+		}
+	}
+	
+	if (found)
+	{
+		skill.uses += 1;
+
+		//Upgrade to lvl 2
+		if (skill.uses == 10 || skill.uses == 40 || skill.uses == 100 || skill.uses == 250)
+		{
+			skill.lvl += 1;
+			printf("\n%s skill has upgraded its level to level %d\n\n", skill.name.c_str(), skill.lvl);
+		}
+	}
+}
 
 
 
 
 
 void TestSkillsSystem() {
-	printf("The skills system doesn't do anything yet...\n");
+
+	SkillSystem MySkills;
+
+	MySkills.AddSkill("Heal");
+
+	for (unsigned int i = 0; i < 260; ++i)
+	{
+		UseSkill(MySkill);
+		printf("%s: lvl:%d | uses:%d\n",MySkill.name.c_str(), MySkill.lvl, MySkill.uses);
+	}
+
 }
--- RPGSystems-Example/SkillsSystem.h
+++ RPGSystems-Example/SkillsSystem.h
@@ -1,4 +1,27 @@
 #pragma once
 #include <stdio.h>
+#include <string>
+#include <vector>
+
+class SkillSystem {
+
+public:
+	std::vector <Skill> SkillList;
+
+	SkillSystem();
+
+	void AddSkill(std::vector <Skill>* SkillList, std::string Name);
+	Skill UseSkill(std::vector <Skill>* SkillList, Skill* skill);
+};
+
+class Skill{
+
+public:
+	std::string name;
+	int lvl;
+	int uses;
+
+	Skill(std::string Name);
+};
 
 void TestSkillsSystem();
