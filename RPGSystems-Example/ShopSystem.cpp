21e3379ca7b14d722261c3eddf470c5afb9afb40
cole wiltzius <colewiltzius@gmail.com>
2021-11-11 11:52:35 -0500

    int sys

--- RPGSystems-Example/ShopSystem.cpp
+++ RPGSystems-Example/ShopSystem.cpp
@@ -1,6 +1,25 @@
+
 #include "ShopSystem.h"
+#include <stdio.h>
+#include <string>
 
+//constructor
+void ShopItem::ShowItemShop()
+{
+	printf("%s\n %d\n %d\n", shopItemName.c_str(), shopItemCost, shopItemQuantity);
+}
 
+ShopItem::ShopItem(std::string ItemName, int ItemCost, int ItemQuantity)
+{
+	shopItemName = ItemName;
+	shopItemCost = ItemCost;
+	shopItemQuantity = ItemQuantity;
+}
+
+ShopItem InstanceofShopItem = ShopItem("Potion", 20, 20)
+{
+	InstanceofShopItem.ShowShopItem();
+}
 
 
 
--- RPGSystems-Example/ShopSystem.h
+++ RPGSystems-Example/ShopSystem.h
@@ -1,4 +1,23 @@
 #pragma once
 #include <stdio.h>
+#include <string>
+
+class ShopItem
+{
+	std::string shopItemName;
+	int shopItemCost;
+	int shopItemQuantity;
+	//method
+	void ShowItemShop();
+	//constructor
+	ShopItem(std::string ItemName, int ItemCost, int ItemQuantity)
+};
+
+class ShopSystem
+{
+public:
+	int playerGold;
+	void BuyItem();
+};
 
 void TestShopSystem();
