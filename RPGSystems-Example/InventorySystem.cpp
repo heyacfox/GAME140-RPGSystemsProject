162d8dd1549d5eb91e4594c3535c013a033fdb76
Dylan Felt <dfelt@GMU.EDU>
2021-11-11 11:44:32 -0500

    We have get item, use item, and drop item functions

--- RPGSystems-Example/InventorySystem.cpp
+++ RPGSystems-Example/InventorySystem.cpp
@@ -1,13 +1,56 @@
 #include "InventorySystem.h"
 
-
-
-
-
-
-
-
-
-void TestInventorySystem() {
-	printf("The inventory system doesn't do anything yet...\n");
+InventorySystem::InventorySystem()
+{
+	InventorySize = 5;
+	InventoryItems = 1;
+	Sword = "SWORD";
+	Apple = "APPLE";
 }
+
+void InventorySystem::CheckInventory() 
+{
+	printf("The amount of items in the inventory is: %d\n", InventoryItems);
+	printf("The size of the inventory is: %d\n", InventorySize);
+}
+
+void InventorySystem::AddToInventory() 
+{
+	InventoryItems = InventoryItems + 1;
+	printf("%s has been picked up\n", Sword.c_str());
+	InventorySize = InventorySize - 1;
+	printf("Inventory size has decreased\n");
+}
+
+void InventorySystem::UseInventory()
+{
+	printf("The %s has been used\n", Sword.c_str());
+	InventorySystem::DropInventory();
+	printf("The %s was dropped\n", Sword.c_str());
+}
+
+void InventorySystem::DestroyInventory() 
+{
+	InventoryItems = InventoryItems - 1;
+	InventorySize = InventorySize + 1;
+}
+
+void InventorySystem::DropInventory()
+{
+	InventoryItems = InventoryItems - 1;
+	InventorySize = InventorySize + 1;
+}
+
+void TestInventorySystem() 
+{
+	InventorySystem inventoryInstance = InventorySystem();
+	inventoryInstance.CheckInventory();
+	inventoryInstance.AddToInventory();
+	inventoryInstance.CheckInventory();
+	inventoryInstance.UseInventory();
+	inventoryInstance.CheckInventory();
+}
+
+//void TestInventorySystem() {
+	//printf("The inventory system doesn't do anything yet...\n");
+//}
--- RPGSystems-Example/InventorySystem.h
+++ RPGSystems-Example/InventorySystem.h
@@ -1,4 +1,27 @@
 #pragma once
 #include <stdio.h>
+#include <string>
+
+class InventorySystem {
+
+public:
+	InventorySystem();
+	virtual void CheckInventory();
+	virtual void AddToInventory();
+	virtual void DropInventory();
+	virtual void UseInventory();
+	virtual void DestroyInventory();
+private:
+	int InventorySize;
+	int InventoryItems;
+	std::string(Sword)
+	{
+
+	};
+	std::string(Apple)
+	{
+		
+	};
+};
 
 void TestInventorySystem();
