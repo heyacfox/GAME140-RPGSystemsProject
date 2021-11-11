0820afa088e9fe5ecd385664cf354390d84867dd
Phillip Andrew Jones <pjones28@GMU.EDU>
2021-11-11 11:54:34 -0500

    Renamed Some System to Equipment Sysytem. Added a 2nd check to see if player has equipment equiped. No has before and after for player stats before and after equiping armour.
    -Team DOB

--- RPGSystems-Example/EquipmentSystem.cpp
+++ RPGSystems-Example/EquipmentSystem.cpp
@@ -1,13 +1,31 @@
+#include "SomeSystem.h"
+#include <stdio.h>
+#include <string>
 #include "EquipmentSystem.h"
 
+EquipmentSystem::EquipmentSystem()
+{
+	defenseInt = 1;
+}
 
+void EquipmentSystem::CheckSystem1() {
+	printf("EquipmentSystem has been checked! No gear equiped! Your defense is % d!\n", defenseInt);
+}
 
+void EquipmentSystem::CheckSystem2() {
+	printf("EquipmentSystem has been checked! Equiping the Steel Chestplate increased defense by % d!\n", defenseInt);
+}
 
-
-
-
-
+void EquipmentSystem::AddToInt() {
+	defenseInt = defenseInt + 9;
+}
 
 void TestEquipmentSystem() {
-	printf("The equipment system doesn't do anything yet...\n");
+	EquipmentSystem someSystemInstance = EquipmentSystem();
+	someSystemInstance.CheckSystem1();
+	someSystemInstance.AddToInt();
+	someSystemInstance.CheckSystem2();
 }
+
+	
+	
--- RPGSystems-Example/EquipmentSystem.h
+++ RPGSystems-Example/EquipmentSystem.h
@@ -1,4 +1,15 @@
 #pragma once
 #include <stdio.h>
 
+class EquipmentSystem {
+
+public:
+	EquipmentSystem();
+	virtual void CheckSystem1();
+	virtual void CheckSystem2();
+	virtual void AddToInt();
+private:
+	int defenseInt;
+};
+
 void TestEquipmentSystem();
