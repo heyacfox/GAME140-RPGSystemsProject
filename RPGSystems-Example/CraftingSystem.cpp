2f72ac5f8b1af913cd3dd6e81751d38b86091cba
Benjamin A Johnson <bjohns47@GMU.EDU>
2021-11-11 11:50:56 -0500

    First push

--- RPGSystems-Example/CraftingSystem.cpp
+++ RPGSystems-Example/CraftingSystem.cpp
@@ -1,13 +1,60 @@
 #include "CraftingSystem.h"
 
+void InitializeGame(PlayerState& playerState)
+{
+	int iron = 2;
+	int ironChestplate = 0;
+	int armoredGloves = 0;
+	int leather = 1;
+	PlayerState.wantsToCraft = false;
+	PlayerState.displayInv = false;
+}
 
+CraftingSystem::CraftingSystem()
+{
+	someInt = 1;
+}
 
+void CraftingSystem::CheckSystem() {
+	printf("SomeSystem has been checked with an int of %d!\n", someInt);
 
+	if (wantsToCraft = true)
+	{
+		diaplayInv = true;
+	{
+		if(ironChestplate)
+		{
+			if (iron >= 1 && leather >= 1)
+			{
+				armoredGloves++;
+				leather--;
+				iron--;
+			}
+		}
+		if (armoredGloves)
+		{
+			if (iron >= 2)
+			{
+				ironChestplate++;
+				iron - 2;
 
+			}
+			else
+			{
+				printf("insufficient items to craft.");
+			}
+		}
+	}
+	//if (someCraftingInstance.CheckSystem(Inventory))
+}
 
-
-
+void CraftingSystem::AddToInt() {
+	someInt = someInt + 1;
+}
 
 void TestCraftingSystem() {
-		printf("The crafting system doesn't do anything yet...\n");
+	CraftingSystem someCraftingInstance = CraftingSystem();
+	someCraftingInstance.CheckSystem();
+	someCraftingInstance.AddToInt();
+	someCraftingInstance.CheckSystem();
 }
--- RPGSystems-Example/CraftingSystem.h
+++ RPGSystems-Example/CraftingSystem.h
@@ -1,4 +1,25 @@
 #pragma once
 #include <stdio.h>
 
+
+
+class CraftingSystem {
+
+public:
+	CraftingSystem();
+	virtual void CheckSystem();
+	virtual void AddToInt();
+	struct PlayerState
+	{
+		bool wantsToCraft;
+		bool displayInv;
+	}
+private:
+	int someInt;
+
+};
+
+
 void TestCraftingSystem();
+void InitializeGame(PlayerState& playerState);
+
