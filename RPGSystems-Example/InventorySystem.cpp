fac41d747fe087bc03c965f9e8ff12d508cb1c32
Dylan Felt <dfelt@GMU.EDU>
2021-11-16 11:42:59 -0500

    Items can be dropped or destroyed, dropped items only can be picked back up, and Quest Items are separate and do not take up inventory space

--- RPGSystems-Example/InventorySystem.cpp
+++ RPGSystems-Example/InventorySystem.cpp
@@ -3,42 +3,77 @@
 InventorySystem::InventorySystem()
 {
 	InventorySize = 5;
-	InventoryItems = 1;
-	Sword = "SWORD";
-	Apple = "APPLE";
+	InventoryItems = 0;
+	PlayerPosition = 1;
+	SwordPosition = 1;
+	ApplePosition = 1;
+	SwordAmount = 0;
+	AppleAmount = 0;
+	Sword = "SWORD(%d)", SwordAmount;
+	Apple = "APPLE(%d)", AppleAmount;
+	Sprite = "SPRITE";
+	QuestAmount = 0;
 }
 
 void InventorySystem::CheckInventory() 
 {
 	printf("The amount of items in the inventory is: %d\n", InventoryItems);
 	printf("The size of the inventory is: %d\n", InventorySize);
+	if (QuestAmount >= 1)
+	{
+		printf("QUEST INVENTORY: %s\n", Sprite.c_str());
+	}
 }
 
 void InventorySystem::AddToInventory() 
 {
-	InventoryItems = InventoryItems + 1;
-	printf("%s has been picked up\n", Sword.c_str());
-	InventorySize = InventorySize - 1;
-	printf("Inventory size has decreased\n");
+	if (SwordPosition == PlayerPosition)
+	{
+		InventoryItems = InventoryItems + 1;
+		printf("%s has been picked up\n", Sword.c_str());
+		InventorySize = InventorySize - 1;
+		SwordAmount = SwordAmount + 1;
+		printf("Inventory size has decreased\n");
+	}
+
+	if (ApplePosition == PlayerPosition)
+	{
+		InventoryItems = InventoryItems + 1;
+		printf("%s has been picked up\n", Apple.c_str());
+		AppleAmount = AppleAmount + 1;
+		InventorySize = InventorySize - 1;
+		printf("Inventory size has decreased\n");
+	}
 }
 
 void InventorySystem::UseInventory()
 {
-	printf("The %s has been used\n", Sword.c_str());
-	InventorySystem::DropInventory();
-	printf("The %s was dropped\n", Sword.c_str());
+	printf("The %s has been used\n", Apple.c_str());
+	InventorySystem::DestroyInventory();
 }
 
 void InventorySystem::DestroyInventory() 
 {
 	InventoryItems = InventoryItems - 1;
 	InventorySize = InventorySize + 1;
+	AppleAmount = AppleAmount - 1;
+	ApplePosition = 0;
 }
 
 void InventorySystem::DropInventory()
 {
 	InventoryItems = InventoryItems - 1;
 	InventorySize = InventorySize + 1;
+	printf("The %s was dropped\n", Sword.c_str());
+	SwordPosition = PlayerPosition;
+	SwordAmount = SwordAmount - 1;
+}
+
+void InventorySystem::QuestInventory()
+{
+	printf("You have accepted the hobo's request\n");
+	printf("You received a %s\n", Sprite.c_str());
+	QuestAmount = 1;
 }
 
 void TestInventorySystem() 
@@ -48,6 +83,12 @@ void TestInventorySystem()
 	inventoryInstance.AddToInventory();
 	inventoryInstance.CheckInventory();
 	inventoryInstance.UseInventory();
+	inventoryInstance.CheckInventory();
+	inventoryInstance.DropInventory();
+	inventoryInstance.CheckInventory();
+	inventoryInstance.AddToInventory();
+	inventoryInstance.CheckInventory();
+	inventoryInstance.QuestInventory();
 	inventoryInstance.CheckInventory();
 }
 
--- RPGSystems-Example/InventorySystem.h
+++ RPGSystems-Example/InventorySystem.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdio.h>
 #include <string>
+#include <vector>
 
 class InventorySystem {
 
@@ -11,16 +12,21 @@ public:
 	virtual void DropInventory();
 	virtual void UseInventory();
 	virtual void DestroyInventory();
+	virtual void QuestInventory();
 private:
 	int InventorySize;
 	int InventoryItems;
-	std::string(Sword)
+	int PlayerPosition;
+	int SwordPosition;
+	int SwordAmount;
+	std::string Sword;
+	int ApplePosition;
+	int AppleAmount;
+	std::string Apple;
+	int QuestAmount;
+	std::string Sprite;
+	struct AppleState
 	{
-
-	};
-	std::string(Apple)
-	{
-		
 	};
 };
 
