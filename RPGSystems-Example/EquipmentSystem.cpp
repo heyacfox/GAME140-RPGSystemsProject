a4ebcad6a469bcab8200fb1423b1d47cada90ae0
Phillip Andrew Jones <pjones28@GMU.EDU>
2021-11-16 12:10:00 -0500

    Added a new System check that lowers your defense stat when un equiping armor.

--- RPGSystems-Example/EquipmentSystem.cpp
+++ RPGSystems-Example/EquipmentSystem.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include "EquipmentSystem.h"
 
+//test equip function 
 EquipmentSystem::EquipmentSystem()
 {
 	defenseInt = 1;
@@ -27,5 +28,29 @@ void TestEquipmentSystem() {
 	someSystemInstance.CheckSystem2();
 }
 
-	
-	
+	//Test unequip function
+EquipmentSystem2::EquipmentSystem2()
+{
+	defenseInt2 = 10;
+}
+
+void EquipmentSystem2::CheckSystem3() {
+	printf("EquipmentSystem has been checked! The Steel Chestplate has increased your defense by % d\n", defenseInt2);
+}
+
+void EquipmentSystem2::SubtractToInt()
+{
+	defenseInt2 = defenseInt2 - 9;
+}
+
+void EquipmentSystem2::CheckSystem4() {
+	printf("EquipmentSystem has been checked! You unequied The Steel Chestplate and your defense has fallen to % d!\n", defenseInt2);
+}
+
+
+void TestEquipmentSystem2() {
+	EquipmentSystem2 someSystemInstance = EquipmentSystem2();
+	someSystemInstance.CheckSystem3();
+	someSystemInstance.SubtractToInt();
+	someSystemInstance.CheckSystem4();
+}
--- RPGSystems-Example/EquipmentSystem.h
+++ RPGSystems-Example/EquipmentSystem.h
@@ -4,12 +4,32 @@
 class EquipmentSystem {
 
 public:
+	//test equip function
 	EquipmentSystem();
 	virtual void CheckSystem1();
 	virtual void CheckSystem2();
 	virtual void AddToInt();
+
 private:
 	int defenseInt;
 };
 
+
+
+class EquipmentSystem2 {
+
+public:
+	//test unequip function
+	EquipmentSystem2();
+			
+	void CheckSystem3();
+	void CheckSystem4();
+	virtual void SubtractToInt();
+
+private:
+	int defenseInt2;
+
+};
+
 void TestEquipmentSystem();
+void TestEquipmentSystem2();
